94ba595cf3df7a257dff09fadce60f20cd120f2c
Haosheng Zhang <haoshengg.zhang@mail.utoronto.ca>
2020-06-14 16:41:18 -0400

    1st version of homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome_Page from './Welcome_Page';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
-function App() {
+class App extends React.Component{
+    render() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+        <div>
+        <BrowserRouter>
+          <Switch> 
+           
+            <Route exact path='/' render={() => 
+                            (<Welcome_Page state={this.state}/>)}/>
+            
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );  
+  }
 }
 
 export default App;
---
+++ src/Welcome_Page/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+
+import "./style.css";
+
+/* Component for the Welcome page */
+class Welcome_Page extends React.Component{
+	render(){
+		return (
+
+			<div className="welcome_page">
+
+				{/* sign up button */}
+				<Link className="button_link" to="" >
+					<Button round className="welcome_button">Want an account!</Button>
+				</Link>
+
+				{/* login button */}
+				<Link className="button_link" to="" >
+					<Button round className="welcome_button">User Login!</Button>
+				</Link>
+
+				{/* guest visit button */}
+				<Link className="button_link" to="" >
+					<Button round className="welcome_button">Continue as a guest!</Button>
+				</Link>
+			</div>
+		);
+	}
+}
+
+export default Welcome_Page;
