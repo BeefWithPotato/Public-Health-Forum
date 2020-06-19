bcc0569ff6b5dac21d669f8abb07567aa047441e
Samarium <28302241+Samarium150@users.noreply.github.com>
2020-06-18 20:36:09 -0400

    update login page

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ import LoginPage from './react-components/LoginPage';
 
 class App extends React.Component{
     render() {
-    return (
-        <div>
-        <BrowserRouter>
-          <Switch> 
-           
-            <Route exact path='/' render={() => 
-                            (<WelcomePage state={this.state} />)}/>
-            <Route exact path='/homepage' render={() =>
-                            (<HomePage state={this.state} />)}/>
-            <Route exact path='/login' render={() => 
-                            (<LoginPage state={this.state} />)}/>
+        return (
+            <div>
+            <BrowserRouter>
+              <Switch>
 
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );  
-  }
+                <Route exact path='/' render={() =>
+                                (<WelcomePage state={this.state} />)}/>
+                <Route exact path='/homepage' render={() =>
+                                (<HomePage state={this.state} />)}/>
+                <Route exact path='/login'>
+                    <LoginPage/>
+                </Route>
+
+              </Switch>
+            </BrowserRouter>
+          </div>
+        );
+    }
 }
 
 export default App;
--- src/react-components/LoginPage/index.js
+++ src/react-components/LoginPage/index.js
@@ -1,31 +1,69 @@
 import React from "react";
 import { BrowserRouter as Router, Link} from "react-router-dom"
-import { Button, TextField, FormGroup } from "@material-ui/core";
+import { Button, TextField, FormGroup, InputAdornment, InputLabel, OutlinedInput, IconButton, FormControl } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@material-ui/icons"
 
 import "./style.css";
 
-const register = React.forwardRef((props, ref) => (
-    <Link ref={ref} to="./RegisterPage" {...props} />
-))
-
 export default function LoginPage() {
 
-    function submit(event) {
+    const [values, setValues] = React.useState({
+        username: '',
+        password: '',
+        show: false
+    });
+
+    const register = React.forwardRef((props, ref) => (
+        <Link ref={ref} to="./RegisterPage" {...props} />
+    ))
+
+    const submit = (event) => {
         event.preventDefault();
+        console.log(values.username+"\n"+values.password)
         /*
         * TODO
         */
     }
+    
+    const change = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    }
+
+    const show = () => {
+        setValues({ ...values, show: !values.show });
+    };
 
     return (
         <div className="login_page">
             <form onSubmit={submit} noValidate autoComplete="off" className="form">
                 <p className="title">Login</p>
                 <FormGroup className="input">
-                    <TextField required variant="outlined" label="Username" placeholder="Username" />
+                    <TextField
+                        required variant="outlined"
+                        label="Username"
+                        placeholder="Username"
+                        onChange={change('username')}
+                    />
                 </FormGroup>
                 <FormGroup className="input">
-                    <TextField required variant="outlined" label="Password" placeholder="Password" />
+                    <FormControl variant="outlined">
+                        <InputLabel>Password *</InputLabel>
+                        <OutlinedInput
+                            required
+                            placeholder="Password"
+                            type={values.show ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={change('password')}
+                            endAdornment={
+                                <InputAdornment position='end'>
+                                    <IconButton onClick={show} edge="end">
+                                        {values.show ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            labelWidth={130}
+                        />
+                    </FormControl>
                 </FormGroup>
                 <FormGroup className="forget">
                     <Button color="primary" size="small">Forget password?</Button>
