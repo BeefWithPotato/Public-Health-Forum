55f0ed76463c6177c22df6c5faf0a2f74998ce7e
Samarium <28302241+Samarium150@users.noreply.github.com>
2020-06-24 15:57:32 -0400

    prepare user authentication function

--- src/App.js
+++ src/App.js
@@ -8,23 +8,34 @@ import LoginPage from './react-components/LoginPage';
 import Post from './react-components/Post';
 
 class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: "NOT_LOGGED_IN",
+            usr: {}
+        }
+    }
+
     render() {
         return (
             <div>
-            <BrowserRouter>
-              <Switch>
-
-                <Route exact path='/' component={WelcomePage}/>
-                <Route exact path='/homepage/:user' component={HomePage}/>
-                <Route exact path='/postpage/:user' component={Post}/>
-                <Route exact path='/login'>
-                    <LoginPage/>
-                </Route>
-                
-
-              </Switch>
-            </BrowserRouter>
-          </div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route
+                          exact path='/'
+                          render={
+                              props => (<WelcomePage {...props} loggedIn={this.state.loggedIn} />)
+                        }
+                        />
+                        <Route exact path='/homepage/:user' component={HomePage}/>
+                        <Route exact path='/postpage/:user' component={Post}/>
+                        <Route exact path='/login'>
+                            <LoginPage/>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
         );
     }
 }
